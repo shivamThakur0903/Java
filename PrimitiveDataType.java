1bfe6a000d9e6d206d49bfc23f2a26c5d0f23e52
Shivam Kumar Thakur <shivamofs2109@gmail.com>
2026-07-06 03:17:26 +0530

    Add PrimitiveDataType class with basic data types

---
+++ PrimitiveDataType.java
@@ -0,0 +1,22 @@
+package com.company;
+
+// Primitive datatype int, short, long, float, double, char, byte, boolean,
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        int age = 21;
+        short weight = 80;
+        long number = 963113;
+        float height = 5.9f;
+        double salary = 35595.50d;
+        char Name = 'S';
+        byte House_No = 102;
+
+        System.out.println("Name Starts With: " + Name);
+        System.out.println("Age is : "+age);
+        System.out.println("Height is : "+height);
+        System.out.println("Weight is : "+weight+"KG");
+        System.out.println("Number is : "+number);
+        System.out.println("Salary is : "+salary);
+        System.out.println("House No. : "+House_No);
+    }
+}
