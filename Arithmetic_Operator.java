c205cd5ea936ce0cb0847a0afab0d5bb3aa383e5
Shivam Kumar Thakur <shivamofs2109@gmail.com>
2026-07-07 23:32:17 +0530

    Arithmetic_Operator all basic operation
    Fixed formatting issues and uncommented sum calculation.

---
+++ Arithmetic_Operator.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Arithmetic_Operator{
+    public static void main(String[] args){
+        int x=10;
+        int y=2;
+
+       /*int sum=x+y;
+        int sub=x-y;
+        int mul=x*y;
+        int div= x/y;
+        int mod= x%y;
+        System.out.println("Addition is: "+sum);
+        System.out.println("Subtraction is: "+sub);
+        System.out.println("Multiplication is: "+mul);
+        System.out.println("Division is: "+div);
+        System.out.println("Modules is: "+mod);
+        */
+
+        System.out.println("\nAddition is: "+(x+y));
+        System.out.println("Subtraction is: "+(x-y));
+        System.out.println("Multiplication is: "+(x*y));
+        System.out.println("Division is: "+(x/y));
+        System.out.println("Modules is: "+(x%y));
+    }
+}
