7140a9bc6090baa9de0119fecb710e5361e9b553
Shivam Kumar Thakur <shivamofs2109@gmail.com>
2026-07-10 01:31:52 +0530

    Add RelationalOperator class with comparison examples

---
+++ RelationalOperator.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class RelationalOperator {
+    public static void main(String[] args){
+        int a=10;
+        int b=100;
+        int c=10;
+
+        System.out.println("a>b : "+(a>b));
+        System.out.println("a<b : "+(a<b));
+        System.out.println("a>=c : "+(a>=c));
+        System.out.println("a<=b : "+(a<=b));
+        System.out.println("a==c : "+(a==c));
+        System.out.println("a!=c : "+(a!=c));
+    }
+}
