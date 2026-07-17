04f10d1cbe71b1b4457b5b07c4514a8fb12a4253
Shivam Kumar Thakur <shivamofs2109@gmail.com>
2026-07-17 22:53:25 +0530

    Add VoteEligibility class to check voting eligibility

---
+++ VoteEligibility.java
@@ -0,0 +1,13 @@
+package com.company;
+public class VoteEligibility{
+    public static void main(String[] args) {
+        int age=22;
+
+        if(age>=18){
+            System.out.println("Eligible for vote.");
+        }
+        else {
+            System.out.println("Not eligible for vote");
+        }
+    }
+}
